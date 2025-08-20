c7da49dbe6c53ad5d446fe7871480a661027afd5
Diksha Bharti <bhartidiksha2008@gmail.com>
2025-08-20 20:49:12 +0530

    Added demo home page

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      {/* HEADER SECTION */}  
+      
+
+      {/* main content section */}
+      <div className='flex flex-col justify-center items-center gap-2'>
+      <h1
+      className='text-5xl font-semibold text-[#07689F]' 
+      >IEEE HOME PAGE
+      </h1>
+      <p
+      className="text-2xl text-[#0E486C]"
+      >(To be designed soon)</p>
+      </div>
+      
+
+      {/* FOOTER SECTION */}
+    </div>
+  );
+}
