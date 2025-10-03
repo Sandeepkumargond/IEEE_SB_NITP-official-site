401eba239584b3ace072b1b3cd85391e5d3b30c9
Prachi <imprachi06@gmail.com>
2025-10-03 20:21:02 +0530

    done with header work

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,13 @@
+import Navbar from './components/Navbar';
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       {/* HEADER SECTION */}  
+      
+   
+     
+        <Navbar/>
+     
       
 
       {/* main content section */}
