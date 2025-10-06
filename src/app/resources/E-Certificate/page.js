f3dc8404d21722d64e8b7c5a06bc1a35ce7b121a
md-aman012 <mdaman13720@gmail.com>
2025-10-06 16:10:41 +0530

    Removed footer and header component

--- src/app/resources/E-Certificate/page.js
+++ src/app/resources/E-Certificate/page.js
@@ -1,6 +1,6 @@
-// import Navbar from "@/components/Navbar";
+
 import CertificateForm from "@/Components/CertificateForm";
-// import Footer from "@/components/Footer";
+
 
 export default function ECertificatePage() {
   return (
