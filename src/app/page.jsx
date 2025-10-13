494c31aa1166c7a18cf30657b94c3d3502ac98ab
Sandeepkumargond <sandeepkumargond198@gmail.com>
2025-10-13 23:00:39 +0530

    fixed some conflicts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,8 @@ export default function Home() {
 
       <div className='flex flex-col  justify-center items-center gap-2'>
         <h1 className='text-5xl font-semibold text-[#07689F]'>IEEE HOME PAGE</h1>
+        <h1 className='text-5xl font-semibold text-[#07689F]'>comming soon</h1>
+
       </div> 
      </div>
   );
