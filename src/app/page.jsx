aec771c68deb915eb802c3dc36fcc2ac38eaa727
md-aman012 <mdaman13720@gmail.com>
2025-11-09 00:01:34 +0530

    Home page updated as new figma design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import CommunitiesSection from "@/components/CommunitiesSection";
-import SocietiesSection from "@/components/SocietiesSection";
+// import SocietiesSection from "@/components/SocietiesSection";
 export default function Home() {
   return (
     <div>
